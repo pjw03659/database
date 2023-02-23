c214a3074ca2745d62db37524a823c21d69b2343
pjw03659 <73214899+pjw03659@users.noreply.github.com>
2023-02-23 17:48:27 +0900

    day19 프로젝트 업로드

---
+++ day19/Script-1.sql
@@ -0,0 +1,21 @@
+CREATE TABLE member3 (
+	"id" varchar2(100),
+	"pw" varchar2(100),
+	"name" varchar2(100),
+	"tel" varchar2(100)
+)
+
+CREATE TABLE bbs2 (
+	"id" varchar2(100),
+	"title" varchar2(100),
+	"content" varchar2(100),
+	"writer" varchar2(100)
+)
+
+CREATE TABLE product2(
+	"no" varchar2(100),
+	"name" varchar2(100),
+	"content" varchar2(100),
+	"price" NUMBER
+	
+)
