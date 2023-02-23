6455259c2e01bd2ef9eac733ea83d9ada8db16bb
pjw03659 <73214899+pjw03659@users.noreply.github.com>
2023-02-23 17:49:14 +0900

    Delete day19 directory

--- day19/Script-2.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "MEMBER2" VALUES ('park','1234','park','011')
-
-INSERT INTO "member" VALUES ('apple','1111','apple','011')
-
-INSERT INTO "member" VALUES ('ice','2222','ice','012')
-
-INSERT INTO "member" VALUES ('melon','3333','melon','013')
