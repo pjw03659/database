cc11ba1f3680aa9de04c11fee00a83c3030a06a7
pjw03659 <73214899+pjw03659@users.noreply.github.com>
2023-03-03 17:57:00 +0900

    day 25 프로젝트 업로드

---
+++ database-workspace/day25/정리문제_0303.sql
@@ -0,0 +1,42 @@
+1. SQL을 3가지로 구분, 각각을 간단 설명 예를 들어보세요.
+-DDL -- 데이터 항목 정의: CREATE, ALTER, DROP
+-DML --데이터 조작: INSERT, SELECT, UPDATE, DELETET
+-DCL --DBMS 제어 
+
+2. DML로 할 수 있는 조작 4가지를 부르는 말
+-CRUD (CREATE, READ, UPDATE, DELETE)
+
+3. DML의 조작 4가지에 해당하는 각 SQL문
+-INSERT, SELECT, UPDATE, DELETE
+
+4.정형데이터베이스란?
+-형태가 변하지 않는 데이터베이스
+
+5.스키마란?
+-틀?
+
+6.제약조건이란? 예 1개이상
+-기본키, NOT NULL
+
+7.PRIMARY KEY를 왜 설정하는가?
+-데이터 일관성을 유지
+-데이터의 모호함을 제거
+-중복 행으로 인한 데이터 꼬임 방지
+
+8.FOREIGN KEY를 왜 설정하는가?
+-외부 테이블에서 참조하기 위해
+-결점 있는 데이터가 들어가는 것을 방지하기 위해
+
+9.저장할 대상을 부르는 말? 한글/영어
+-개체 / ENTITY
+
+10.VARCHAR2와 CHAR의 차이
+-VARCHAR2는 크기가 정해져있지 않음
+
+11.JOIN을 왜 하는가? 
+-검색을 하고 싶은 항목이 여러 테이블에 흩어져 있을 경우
+
+12.JOIN의 3가지 종류 비교
+-INNER JOIN : 중복되는 데이터만 표시
+-LEFT OUTER JOIN : 왼쪽 기준으로 데이터 표시
+-RIGHT OUTER JOIN : 오른쪽 기준으로 데이터 표시
